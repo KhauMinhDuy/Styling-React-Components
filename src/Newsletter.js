2becd1e228252274acba0de63346c2fd4485ecb9
khauminhduy <khauminhduy@gmail.com>
2022-07-09 23:11:16 +0700

    init branch

--- src/Newsletter.js
+++ src/Newsletter.js
@@ -1,31 +1,5 @@
 import React from "react";
-
-function Newsletter(props) {
-  const [email, setEmail] = React.useState("");
-  // const emailPartsCount = countEmailParts(email)
-  return (
-    <section style={styles.container()}>
-      <div style={styles.spectrum()} aria-hidden>
-        {Array.from(Array(5)).map((_, i) => (
-          <div style={styles.bar({ i })} key={i}></div>
-        ))}
-      </div>
-      <header style={styles.header()}>
-        <h2 style={styles.headerH2()}>Get the newsletter</h2>
-      </header>
-      <input
-        style={styles.email()}
-        type="email"
-        placeholder="Your email"
-        value={email}
-        onChange={(evt) => setEmail(evt.target.value)}
-      />
-      <button style={styles.submit()}>Sign up</button>
-    </section>
-  );
-}
-
-export default Newsletter;
+import styled from "styled-components";
 
 const color = {
   spectrum1: "#ff598a",
@@ -35,78 +9,128 @@ const color = {
   spectrum5: "#5e9fff",
 };
 
-const styles = {
-  container: () => ({
-    position: "relative",
-    maxWidth: "100%",
-    fontSize: "1.25em",
-    padding: "1em 1em 2em 1em",
-    background: "#2b283d",
-  }),
-  spectrum: () => ({
-    position: "absolute",
-    top: "0",
-    bottom: "0",
-    left: "0",
-    width: "100%",
-    display: "flex",
-    alignItems: "flex-end",
-    pointerEvents: "none",
-  }),
-  bar: ({ i }) => ({
-    height: "0.5em",
-    width: "20%",
-    transformOrigin: "bottom",
-    transition: "all 1s",
-    background: color[Object.keys(color)[i % Object.keys(color).length]],
-  }),
-  header: () => ({
-    position: "relative",
-    color: "white",
-    zIndex: "1",
-    textTransform: "uppercase",
-    fontSize: "0.85em",
-    textShadow: "0 3px 2px #000",
-  }),
-  headerH2: () => ({
-    margin: "0 0 0.5em 0",
-  }),
-  email: () => ({
-    position: "relative",
-    height: "2em",
-    lineHeight: "2em",
-    fontSize: "0.85em",
-    padding: "0 0.5em",
-    width: "100%",
-    margin: "0.15em",
-    border: "1px solid black",
-    color: "inherit",
-    background: "inherit",
-    textAlign: "inherit",
-    outlineOffset: "0.15em",
-    outline: "none",
-  }),
-  submit: () => ({
-    position: "absolute",
-    left: "50%",
-    bottom: "0",
-    overflow: "hidden",
-    padding: "0",
-    margin: "0",
-    background: "transparent",
-    border: "0",
-    borderBottom: 0,
-    textTransform: "uppercase",
-    transition: "all 300ms",
-    fontSize: "0",
-    zIndex: "1",
-    color: "#070222",
-    fontWeight: "bold",
-    cursor: "pointer",
-    outlineOffset: "4px",
-    outline: "none",
-  }),
-};
+const Container = styled.section`
+  position: relative;
+  max-width: 100%;
+  font-size: 1.25em;
+  padding: 1em 1em 2em 1em;
+  background: #2b283d;
+`;
+
+const Header = styled.header`
+  position: relative;
+  color: #fff;
+  z-index: 1;
+  text-transform: uppercase;
+  font-size: 0.85em;
+  text-shadow: 0 3px 2px #000;
+
+  h2 {
+    margin: 0 0 0.5em 0;
+  }
+`;
+
+const Email = styled.input`
+  position: relative;
+  height: 2em;
+  line-height: 2em;
+  font-size: 0.85em;
+  padding: 0 0.5em;
+  width: 100%;
+  margin: 0.15em;
+  border: 1px solid black;
+  color: inherit;
+  background: inherit;
+  text-align: inherit;
+`;
+
+const Submit = styled.button`
+  position: absolute;
+  left: 50%;
+  z-index: 1;
+  overflow: hidden;
+  margin: 0;
+  background: transparent;
+  color: #070222;
+  background: #fff;
+  font-weight: bold;
+  border: 0;
+  cursor: pointer;
+  text-transform: uppercase;
+  transition: all 300ms;
+  transform: translateX(-50%) rotate(0deg);
+  bottom: auto;
+  height: 0;
+  width: 0;
+  font-size: 0;
+  padding: 0;
+  border-bottom: 0;
+`;
+
+const Spectrum = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  align-items: flex-end;
+  pointer-events: none;
+`;
+
+const Bar = styled.div`
+  height: 0.5em;
+  width: 20%;
+  transform-origin: bottom;
+  transition: all 1s;
+
+  &:nth-child(1n) {
+    background: ${color.spectrum1};
+    animation-delay: 0;
+  }
+  &:nth-child(2n) {
+    background: ${color.spectrum2};
+    animation-delay: 50ms;
+  }
+  &:nth-child(3n) {
+    background: ${color.spectrum3};
+    animation-delay: 100ms;
+  }
+  &:nth-child(4n) {
+    background: ${color.spectrum4};
+    animation-delay: 150ms;
+  }
+  &:nth-child(5n) {
+    background: ${color.spectrum5};
+    animation-delay: 200ms;
+  }
+`;
+
+function Newsletter(props) {
+  const [email, setEmail] = React.useState("");
+  // const emailPartsCount = countEmailParts(email)
+  return (
+    <Container>
+      <Spectrum aria-hidden>
+        {Array.from(Array(5)).map((_, i) => (
+          <Bar key={i}></Bar>
+        ))}
+      </Spectrum>
+      <Header>
+        <h2>Get the newsletter</h2>
+      </Header>
+      <Email
+        type="email"
+        placeholder="Your email"
+        value={email}
+        onChange={(evt) => setEmail(evt.target.value)}
+      />
+      <Submit>Sign up</Submit>
+    </Container>
+  );
+}
+
+export default Newsletter;
 
 // function countEmailParts(email) {
 //   if (/@.+\..{2,}$/.test(email)) {
